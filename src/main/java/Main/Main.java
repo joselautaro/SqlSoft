9e1741f3e8dd38521996ce059e30cf6fe46810a5
Lautaro Montaña <joselautarom@gmail.com>
2023-10-25 08:34:51 -0300

    Conexión con software de gestión mysql

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,12 +1,18 @@
 
 package Main;
 
+import java.sql.SQLException;
+
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Conexion con = new Conexion();
         
-        con.establecerConeccion();
+        //con.establecerConeccion();
+        con.insertarDatos();
+        //con.actualizarDato();
+        //con.consultarDatos();
+        //con.eliminarDato();
     }
     
 }
